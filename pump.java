737c74832bb90da94802e8278256c5665f3528ce
Air <arihanairyadav@gmail.com>
2021-11-26 13:13:02 -0800

    Milk Pumping (10-10)

--- pump.java
+++ pump.java
@@ -36,16 +36,12 @@ public class pump {
     public static int m;
     public static int n;
     public static void main(String[] args) throws IOException{
-        Kattio s = new Kattio();
+        Kattio s = new Kattio("pump");
 
         n = s.nextInt();
         m = s.nextInt();
 
-        dist = new int[n + 1];
-
-        for (int i = 1; i <= n; i++) {
-            dist[i] = Integer.MAX_VALUE;
-        }
+		double best = 0;
 
         arr = new ArrayList [n + 1];
 
@@ -56,22 +52,62 @@ public class pump {
         for (int i = 0; i < m; i++) {
             int one = s.nextInt();
             int two = s.nextInt();
-            int weight = s.nextInt();
+			int cost = s.nextInt();
+            int flow = s.nextInt();
 
-            trip t = new trip(two, weight);
+            trip t = new trip(two, cost, flow, one);
+			trip to = new trip(one, cost, flow, two);
 
             arr[one].add(t);
+			arr[two].add(to);
         }
 
-        dijikstra(1);
+        int counter = 0;
 
-        for (int i = 1; i <= n; i++) {
-            System.out.print(dist[i] + " ");
-        }
+		while (counter < m) {
+			trip curf = dijikstra(1);
+
+			int cost = dist[n];
+
+			if (cost == Integer.MAX_VALUE) {
+				break;
+			}
+
+			double rn = ((double) curf.f) / ((double) cost);
+
+			if (rn > best) {
+				best = rn;
+			}
+
+			ArrayList<trip> temp = new ArrayList <trip>();
+
+			for (trip k : arr[curf.b]) {
+				if (k.equals(curf)) {
+					continue;
+				}
+
+				temp.add(k);
+			}
+
+			arr[curf.b] = temp;
+
+			counter++;
+		}
+
+		s.println((int) (best * 1000000));
+		s.close();
     }
 
-    public static void dijikstra (int x) {
+    public static trip dijikstra (int x) {
         boolean [] used = new boolean[n + 1];
+
+		dist = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            dist[i] = Integer.MAX_VALUE;
+        }
+
+		trip min = new trip(0, 0, Integer.MAX_VALUE, 0);
 
         dist[x] = 0;
 
@@ -90,14 +126,20 @@ public class pump {
 
             for (trip t : arr[a]) {
                 int b = t.a;
-                int w = t.w;
+                int w = t.c;
 
                 if (dist[a] + w < dist[b]) {
                     dist[b] = dist[a] + w;
                     q.add(new Pair(dist[b], b));
+
+					if (t.f < min.f) {
+						min = t;
+					}
                 }
             }
         }
+
+		return min;
     }
 
     static class Pair implements Comparable <Pair> {
@@ -115,18 +157,31 @@ public class pump {
         }
     }
 
-    static class trip {
+    static class trip implements Comparable <trip> {
         int a;
-        int w;
+        int c;
+		int f;
+		int b;
 
-        public trip (int x, int z) {
+        public trip (int x, int z, int a, int b) {
             this.a = x;
-            this.w = z;
+            this.f = a;
+			this.c = z;
+			this.b = b;
         }
 
         @Override
         public String toString () {
-            return a + ", " + w + ":";
+            return a + ", " + f + ", " + c + ", " + b + ":";
         }
+
+		@Override
+		public int compareTo(trip o) {
+			if (this.a == o.a && this.f == o.f && this.c == o.c) {
+				return 1;
+			}
+
+			return 0;
+		}
     }
 }
